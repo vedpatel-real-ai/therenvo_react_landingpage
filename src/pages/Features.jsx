ec44476c263bc99816e63bea14825a99c049cea1
Ved Patel <vedpatel.ai@gmail.com>
2026-07-19 02:53:16 +0530

    Update Features.jsx

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './features.css';
+import './Features.css';
 
 const Features = () => {
   const [activeSection, setActiveSection] = useState('');
